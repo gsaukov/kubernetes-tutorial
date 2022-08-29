49e3aa14bed94d979894a1b43ed7c0a1ce629577
Georgy Saukov <georgy.saukov@sap.com>
2022-08-29 14:03:36 +0200

    Error controller.

---
+++ kt-sym/src/main/java/com/kt/sym/cart/controller/ErrorController.java
@@ -0,0 +1,47 @@
+package com.kt.sym.cart.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Cart calculation simulation has failed", e);
+        return new ErrorResponse().addError("Cart calculation simulation has failed")
+                .getResponse(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private static class ErrorResponse {
+
+        private HttpStatus status;
+        private final List<String> messages = new ArrayList<>();
+
+        public ErrorResponse addError(String error) {
+            messages.add(error);
+            return this;
+        }
+
+        public ResponseEntity<ErrorResponse> getResponse(HttpStatus status) {
+            this.status = status;
+            return new ResponseEntity(this, status);
+        }
+
+        public HttpStatus getStatus() {
+            return status;
+        }
+
+        public List<String> getMessages() {
+            return messages;
+        }
+
+    }
+}
